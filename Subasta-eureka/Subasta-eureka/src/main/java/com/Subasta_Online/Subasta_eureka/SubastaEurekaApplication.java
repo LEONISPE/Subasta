280b491d4600509c0bd62ea4ef88b5042232f873
Leo nispe <leomoisesnisperuzaamaya7@gmail.com>
2025-05-08 16:20:08 -0500

    registrando los microservcios a eureka y enrutando las peticiones con el api gateway

--- Subasta-eureka/Subasta-eureka/src/main/java/com/Subasta_Online/Subasta_eureka/SubastaEurekaApplication.java
+++ Subasta-eureka/Subasta-eureka/src/main/java/com/Subasta_Online/Subasta_eureka/SubastaEurekaApplication.java
@@ -2,8 +2,10 @@ package com.Subasta_Online.Subasta_eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SubastaEurekaApplication {
 
 	public static void main(String[] args) {
