91209b78bc589cbe6239be85a2bc2cb75a81fcd3
Leo nispe <leomoisesnisperuzaamaya7@gmail.com>
2025-08-12 15:12:40 -0500

    Fix 9 ruta correcta para pom.xml

--- Subasta-usuario/Subasta-usuario/src/test/java/com/subastapuja/Subasta_usuario/service/ParticipacionServiceTest.java
+++ Subasta-usuario/Subasta-usuario/src/test/java/com/subastapuja/Subasta_usuario/service/ParticipacionServiceTest.java
@@ -23,8 +23,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 
-@SpringBootTest
-@ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 class ParticipacionServiceTest {
 
