eca1e319ed5b2f6e910e588bca0a2bf739939e5a
Leo nispe <leomoisesnisperuzaamaya7@gmail.com>
2025-08-12 15:23:36 -0500

    Fix 11 ruta correcta para pom.xml

--- Subasta-usuario/Subasta-usuario/src/test/java/com/subastapuja/Subasta_usuario/SubastaUsuarioApplicationTests.java
+++ Subasta-usuario/Subasta-usuario/src/test/java/com/subastapuja/Subasta_usuario/SubastaUsuarioApplicationTests.java
@@ -2,7 +2,10 @@ package com.subastapuja.Subasta_usuario;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+
+@ActiveProfiles("test")
 @SpringBootTest
 class SubastaUsuarioApplicationTests {
 
